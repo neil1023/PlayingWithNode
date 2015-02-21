7c15451aeb2c4ed26b5c300b8a272535fac99b7b
Neil Patel <neil1023@yahoo.com>
2015-02-21 16:50:19 -0500

    particle style + javascript added

---
+++ public/javascripts/demo.js
@@ -0,0 +1,14 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#fff',
+    lineColor: '#fff'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
